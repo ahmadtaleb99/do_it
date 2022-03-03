d6a5b8032b2067ae7ca5abb11b615764279919b8
unknown <ahmad.taleb1@hotmail.com>
2022-03-03 16:47:24 +0200

    'initital4'

--- lib/view/widgets/List_screen/BuildTaskItems.dart
+++ lib/view/widgets/List_screen/BuildTaskItems.dart
@@ -3,6 +3,7 @@ import 'package:do_it/view/widgets/List_screen/TaskCard.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:inview_notifier_list/inview_notifier_list.dart';
 import 'package:visibility_detector/visibility_detector.dart';
 
 class BuildTaskItems extends StatelessWidget {
@@ -11,9 +12,49 @@ class BuildTaskItems extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(itemBuilder: (context,index){
-       double opacity = 1.0;
-      return TaskCard(index: index,);
-    }, separatorBuilder: (context,index) => SizedBox(height: 20.h,), itemCount: 17);
+    // return ListView.separated(itemBuilder: (context,index){
+    //    double opacity = 1.0;
+    //   return TaskCard(index: index,);
+    // }, separatorBuilder: (context,index) => SizedBox(height: 20.h,), itemCount: 17);
+
+
+
+
+
+   return InViewNotifierList(
+      isInViewPortCondition:
+          (double deltaTop, double deltaBottom, double viewPortDimension) {
+            return deltaTop < (0.7 * viewPortDimension) ;
+      },
+      itemCount: 10,
+      builder: (BuildContext context, int index) {
+        return InViewNotifierWidget(
+          id: '$index',
+          builder: (BuildContext context, bool isInView, Widget  ? child) {
+            // return Container(
+            //   height: 250.0,
+            //   color: isInView ? Colors.green : Colors.red,
+            //   child: Text(
+            //     isInView ? 'Is in view' : 'Not in view',
+            //   ),
+            // );
+
+              return TaskCard(index: index,isInView: isInView);
+
+          },
+        );
+      },
+    );
+
+
+
+
+
+
+
+
+
+
+
   }
 }
